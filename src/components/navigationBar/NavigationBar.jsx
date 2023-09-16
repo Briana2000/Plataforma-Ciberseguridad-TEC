1eaf7aa608f301d88789aa65b884cb16dadf8d56
Briana Monge <brianamonge@hotmail.com>
2023-09-16 00:51:57 -0600

    Se agregó interfaz para opcion de Submit Flag

--- src/components/navigationBar/NavigationBar.jsx
+++ src/components/navigationBar/NavigationBar.jsx
@@ -1,7 +1,15 @@
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import './navigationBar.css';
 
 const NavigationBar = () => {
+  
+  const [activeItem, setActiveItem] = useState('challenges'); // Inicialmente, Challenges está activo
+
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
       <a className="navbar-brand custom-margin-right" href="#">TEC</a>
@@ -10,11 +18,23 @@ const NavigationBar = () => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
-          <li className="nav-item active">
-          <Link className="nav-link" to="/challenges">Challenges <span className="sr-only"></span></Link>
+          <li className={`nav-item ${activeItem === 'challenges' ? 'active' : ''}`}>
+            <Link
+              className="nav-link"
+              to="/challenges"
+              onClick={() => handleItemClick('challenges')}
+            >
+              Challenges <span className="sr-only"></span>
+            </Link>
           </li>
-          <li className="nav-item">
-          <Link className="nav-link" to="/submit_flag"> Submit Flag <span className="sr-only"></span></Link>
+          <li className={`nav-item ${activeItem === 'submit_flag' ? 'active' : ''}`}>
+            <Link
+              className="nav-link"
+              to="/submit_flag"
+              onClick={() => handleItemClick('submit_flag')}
+            >
+              Submit Flag <span className="sr-only"></span>
+            </Link>
           </li>
         </ul>
       </div>
--- src/pages/submitFlag/SubmitFlagPage.jsx
+++ src/pages/submitFlag/SubmitFlagPage.jsx
@@ -1,10 +1,38 @@
+import React, { useState } from 'react';
 
 export const SubmitFlagPage = () => {
+
+  const [flag, setFlag] = useState('');
+
+  const handleFlagChange = (event) => {
+    setFlag(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes enviar la flag al servidor o realizar validaciones adicionales.
+    console.log('Flag submitted:', flag);
+  };
+
   return (
-    <div>
-      
+    <div className="container">
+      <h2 className="mt-4">Submit a flag</h2>
+      <div className="row">
+        <div className="col-md-6 mt-2">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter flag"
+            value={flag}
+            onChange={handleFlagChange}
+          />
+        </div>
+        <div className="col-md-6 mt-2">
+          <button type="submit" className="btn btn-primary">Check</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 
